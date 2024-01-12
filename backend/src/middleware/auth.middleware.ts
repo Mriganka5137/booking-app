87a9cc870cbeaca6fe74136b69ea47a9860ca264
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-01-12 12:32:04 +0530

    Add userId to Express Request interface in auth.middleware.ts

--- backend/src/middleware/auth.middleware.ts
+++ backend/src/middleware/auth.middleware.ts
@@ -1,4 +1,13 @@
 import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
 
 export const verifyToken = (
   req: Request,
@@ -6,4 +15,15 @@ export const verifyToken = (
   next: NextFunction
 ) => {
   const token = req.cookies(["auth_token"]);
+  if (!token) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.userId = (decoded as JwtPayload).userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "unauthorized" });
+  }
 };
