fda63c0bb69dcb9c164d93c6d735a2debea42746
Mriganka Mousum Gogoi <gogoimriganka09@gmail.com>
2024-01-12 12:32:00 +0530

    Fix import paths in user.controller and user.routes

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { User } from "../models/user.model";
+import { User } from "../model/user.model";
 
 import jwt from "jsonwebtoken";
 import { UserSchema } from "../utils/validation";
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser } from "../controllers/user.controller";
+import { registerUser } from "../controller/user.controller";
 const router = express.Router();
 
 router.post("/register", registerUser);
